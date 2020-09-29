910b616c9b550569b215b0843a23b7f4dc97c55d
Alex M <xbestyan@hotmail.com>
2020-09-29 20:58:18 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doska.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doska.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICatalogRepository repository;
+        public HomeController(ICatalogRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(repository.Catalogs);
+        }
+    }
+}
---
+++ Models/CatalogRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doska.Models
+{
+    public class CatalogRepository : ICatalogRepository
+    {
+        public IEnumerable<Catalog> Catalogs => new List<Catalog> { 
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Вакансии" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Ищут работу" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Курсы, образованме" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Деловые контакты" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Юридические услуги" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Финансовые услуги" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Переводы текстов" },
+            new Catalog { Title = "Работа и бизнес", Subtitle = "Разное" },
+            new Catalog { Title = "Эектротехника", Subtitle = "Телефоны и связь" },
+            new Catalog { Title = "Эектротехника", Subtitle = "Бытовая техника" },
+            new Catalog { Title = "Эектротехника", Subtitle = "Компьютеры, оргтехника" },
+
+        };
+    }
+}
