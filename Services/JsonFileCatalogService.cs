9a50f1d8fb9319deff8f2acf21e27b391a8802d7
Alex M <xbestyan@hotmail.com>
2020-10-01 21:39:25 +0300

    add new json data

--- Services/JsonFileCatalogService.cs
+++ Services/JsonFileCatalogService.cs
@@ -8,6 +8,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Text;
 
 namespace Doska.Services
 {
@@ -36,15 +37,15 @@ namespace Doska.Services
                     });
             }
         }
-                public void WriteCatalogs()
-        {
-            using (FileStream fs = File.OpenWrite(JsonFileName))
-            {
-                string output = JsonConvert.SerializeObject(repository.Catalogs);
-                byte[] bytes = Encoding.Default.GetBytes(output);
-                fs.Write(bytes, 0, bytes.Length);
-            }
-        }
+        //public void WriteCatalogs()
+        //{
+        //    using (FileStream fs = File.OpenWrite(JsonFileName))
+        //    {
+        //        string output = JsonConvert.SerializeObject(repository.Catalogs);
+        //        byte[] bytes = Encoding.Default.GetBytes(output);
+        //        fs.Write(bytes, 0, bytes.Length);
+        //    }
+        //}
 
         //public void AddRating(string productId, int rating)
         //{
