500e87e099701e0aec9ce3ddc36892ccc96fe6de
Alex Muhortov <44553240+MuhortovAA@users.noreply.github.com>
2020-12-03 17:23:47 +0300

    Update AdsesListViewModel.cs
    update class

--- Models/ViewModels/AdsesListViewModel.cs
+++ Models/ViewModels/AdsesListViewModel.cs
@@ -7,7 +7,10 @@ namespace Doska.Models.ViewModels
 {
     public class AdsesListViewModel
     {
-        public IEnumerable<Ads> Adses { get; set; }
+        //public IEnumerable<Ads> Adses { get; set; }
+        public vCatalog Catalog { get; set; }
+        public IEnumerable<AdsSelect> Adses { get; set; }
+
         public PagingInfo PagingInfo { get; set; }
     }
 }
