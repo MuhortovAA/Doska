6857ad083df73c8d769baa58dde449de2ced5c44
Alex Muhortov <44553240+MuhortovAA@users.noreply.github.com>
2020-12-04 15:38:42 +0300

    Update ICatalogRepository.cs

--- Models/ICatalogRepository.cs
+++ Models/ICatalogRepository.cs
@@ -15,10 +15,13 @@ namespace Doska.Models
         vCatalog GetCatalog(int id);
         void CreateAds(Ads ads);
         List<Ads> GetCustomerAdses(string Id);
+        List<AdsCustomer> GetCustomerAdses2(string Id);
+
         List<Ads> GetAdses(int Id);
         List<AdsSelect> GetAdses2(int Id);
         int GetCountAdses(int Id);
         List<AdsFind> GetAdses2(string findtext);
+        Ads GetAds(string id);
 
 
     }
