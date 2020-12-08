5690408ad1bd6126c658168be2f4ea0ccdd75745
Alex Muhortov <44553240+MuhortovAA@users.noreply.github.com>
2020-12-08 13:38:31 +0300

    Update CreateModel.cs

--- Models/ViewModels/CreateModel.cs
+++ Models/ViewModels/CreateModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -16,5 +16,39 @@ namespace Doska.Models.ViewModels
         [Required]
         [UIHint("password")]
         public string Password { get; set; }
+        
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+
+    public class ViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+    public class EditModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        //[Required]
+        //[UIHint("password")]
+        //public string Password { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
     }
 }
